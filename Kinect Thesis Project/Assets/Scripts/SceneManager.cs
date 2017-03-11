88ea2d7a39bfb0ce8e6298b2636a1b918a78c901
Alexandros Panagiotis Giakalis <agiaka15@student.aau.dk>
2017-03-11 15:11:38 +0100

    Kinect v2 implementation

---
+++ Kinect Thesis Project/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+
+    public static SceneManager instance;
+
+    public GameObject player1, player2;
+    public Material player1mat, player2mat;
+
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+}
